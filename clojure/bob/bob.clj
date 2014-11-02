0a9f6ff0d9f9bd43147c3e16de2855aec735bd99
Kedar <kedardeore@gmail.com>
2014-11-02 11:20:23 +0530

    Alternative bob.clj

--- clojure/bob/bob.clj
+++ clojure/bob/bob.clj
@@ -3,5 +3,11 @@
 
 (defn response-for [call]
   (if (str/blank? call) "Fine. Be that way!"
-    (if (= call (str/upper-case call)) "Woah, chill out!"
-      (if (= \? (.charAt call (- (.length call) 1))) "Sure." "Whatever."))))
+    (if (= \? (.charAt call (- (.length call) 1))) "Sure."
+      (if (every? #(or (not (Character/isLetter %))
+                       (Character/isUpperCase %))
+              call) "Woah, chill out!" "Whatever."
+        )
+      )
+    )
+  )
